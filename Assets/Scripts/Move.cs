5e8d105c655719670fa73605cab66ae0338ec6cf
Alex Gorevski <algore@microsoft.com>
2016-03-26 16:12:18 -0700

    Change line to white sprite.  Add public color defaulted to red, add trigger to change it to green if you hit spacebar in the correct region.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Move : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
     public int force;
+    public Color color;
+
+    private SpriteRenderer spRend;
+    private bool inRange;
+    private Rigidbody2D rb2d;
     private bool initForce;
 
     // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spRend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,5 +26,23 @@ public class Move : MonoBehaviour
             rb2d.AddForce(new Vector2(force, 0));
             initForce = true;
         }
+
+        if (inRange && Input.GetKeyDown(KeyCode.Space))
+        {
+
+            spRend.color = Color.green;
+            //spRend.enabled = false;
+        }
     }
+
+    void OnTriggerEnter2D()
+    {
+        inRange = true;
+    }
+
+    void OnTriggerExit2D()
+    {
+        inRange = false;
+    }
+
 }
