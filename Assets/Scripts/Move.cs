b48525cd19182484a370e6c668f89970dc66ebd5
Alex Gorevski <algore@microsoft.com>
2016-03-26 15:44:19 -0700

    Delete complex sprites & animations.  Add simple crosshair, simple line, and movement script
    Add mov

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int force;
+    private bool initForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initForce)
+        {
+            rb2d.AddForce(new Vector2(force, 0));
+            initForce = true;
+        }
+    }
+}
