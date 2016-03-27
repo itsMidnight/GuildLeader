71ac0bd132b5e7354763609b2ac8a58d00e0aecb
Shannon <snopromise@gmail.com>
2016-03-27 14:08:52 -0700

    Update game stat manager shell

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class GameDataManager
+{
+    RaidManager raids;
+    MemberManager members;
+    public GameDataManager()
+    {
+
+    }
+}
+
 public class GameStateManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameDataManager manager;
+    private static GameStateManager instance = null;
+    public static GameStateManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StartGame()
+    {
+        this.manager = new GameDataManager();
+    }
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
