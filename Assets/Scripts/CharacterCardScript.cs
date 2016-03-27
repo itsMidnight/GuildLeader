360619db12f07b49747b12e4b4a958df6bb11797
Shannon <snopromise@gmail.com>
2016-03-26 17:10:19 -0700

    moving on up

---
+++ Assets/Scripts/CharacterCardScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCardScript : MonoBehaviour {
+    public string characterName; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
