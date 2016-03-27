0f8d7e99c9cd72639485480b71abc622f7075b05
Shannon <snopromise@gmail.com>
2016-03-27 13:06:43 -0700

    Level manager and loading

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void onClick()
+    {
+        LevelStateManager m = GameObject.FindGameObjectWithTag("LevelMgr").GetComponent<LevelStateManager>();
+        SceneManager.LoadScene("crosshairs");
+    }
+}
