69905e6543220a2cc3ad96ffadbf875b29208c55
Jessica A <Jessica A>
2016-03-27 13:38:30 -0700

    update sprites and added weapons/eyes to character generator

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -12,19 +12,65 @@ public class Character : MonoBehaviour
         Darkest = 4
     }
 
+	public enum CharacterEyes
+	{
+		Blue = 5,
+		Green = 6,
+		Brown = 7,
+		Orange = 8,
+		Black = 9
+	}
+
+	public enum CharacterWeapon
+	{
+		SwordSheild = 10,
+		Daggers = 11,
+		Spear = 12,
+		Bow = 13,
+		Staff = 14,
+		Magic = 15
+	}
+
     public enum CharacterSuit
     {
-        A = 18,
-        B = 19,
-        C = 20,
-        D = 21,
-        E = 22,
-        F = 23,
-        G = 24,
-        H = 25,
-        I = 26,
-        J = 27,
-        K = 28,
+        LightArmor1 = 16,
+        MediumArmor1 = 17,
+        HeavyArmor1 = 18,
+        Mage1 = 19,
+        Cleric1 = 20,
+        Glamazon1 = 21,
+        Ranger1 = 22,
+        Ninja1 = 23,
+        SuperNinja1 = 24,
+        Mario1 = 25,
+        Cape1 = 26,
+		Baby1 = 27,
+
+		LightArmor2 = 28,
+		MediumArmor2 = 29,
+		HeavyArmor2 = 30,
+		Mage2 = 31,
+		Cleric2 = 32,
+		Glamazon2 = 33,
+		Ranger2 = 34,
+		Ninja2 = 35,
+		SuperNinja2 = 36,
+		Mario2 = 37,
+		Cape2 = 38,
+		Baby2 = 39,
+
+		LightArmor3 = 40,
+		MediumArmor3 = 41,
+		HeavyArmor3 = 42,
+		Mage3 = 43,
+		Cleric3 = 44,
+		Glamazon3 = 45,
+		Ranger3 = 46,
+		Ninja3 = 47,
+		SuperNinja3 = 48,
+		Mario3 = 49,
+		Cape3 = 50,
+		Baby3 = 51
     }
 
 
@@ -34,13 +80,17 @@ public class Character : MonoBehaviour
     private Sprite[] characterSprites;
     private SpriteRenderer skin;
     private SpriteRenderer suit;
+	private SpriteRenderer weapon;
+	private SpriteRenderer eyes;
 
     public void Init()
     {
         var spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-        characterSprites = Resources.LoadAll<Sprite>("Sprites/Guildies");
+		characterSprites = Resources.LoadAll<Sprite>("Sprites/Guildies");
         skin = spriteRenderers[0];
         suit = spriteRenderers[1];
+		weapon = spriteRenderers[2];
+		eyes = spriteRenderers[3];
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -68,17 +118,29 @@ public class Character : MonoBehaviour
         return (Character.CharacterSuit)suits.GetValue((int)Random.Range(0, suits.Length - 0.00001f));
     }
 
+	public CharacterWeapon GetRandomWeapon()
+	{
+		var weapons = System.Enum.GetValues(typeof(Character.CharacterWeapon));
+		return (Character.CharacterWeapon)weapons.GetValue((int)Random.Range(0, weapons.Length - 0.00001f));
+	}
+
+	public CharacterEyes GetRandomEyes()
+	{
+		var eyes = System.Enum.GetValues(typeof(Character.CharacterEyes));
+		return (Character.CharacterEyes)eyes.GetValue((int)Random.Range(0, eyes.Length - 0.00001f));
+	}
+
     public void MoveAcrossScreen(int force)
     {
         var multiplier = Random.Range(0.5f, 2f);
         rigidBody.AddForce(new Vector2(force * multiplier, 0));
     }
 
-    public void InitializeLookAndFeel(CharacterSkinTone skintone, CharacterSuit suitStyle)
+    public void InitializeLookAndFeel(CharacterSkinTone skintone, CharacterSuit suitStyle, CharacterWeapon weaponStyle, CharacterEyes eyeColor)
     {
         skin.sprite = characterSprites[(int)skintone];
         suit.sprite = characterSprites[(int)suitStyle];
+		weapon.sprite = characterSprites[(int)weaponStyle];
+		eyes.sprite = characterSprites[(int)eyeColor];
     }
-
-
 }
