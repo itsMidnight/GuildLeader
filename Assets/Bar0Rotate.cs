c7ce4650e7f7ae6c1eef17a46beb2ed4aa21be3c
nonskanse <victoria.house@gmail.com>
2016-03-26 19:24:53 -0700

    Entering a Tank minigame Trying first commit eek

--- Assets/Bar0Rotate.cs
+++ Assets/Bar0Rotate.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Bar0Rotate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +10,6 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate (0, 0, Time.deltaTime * 10 );
 	}
 }
