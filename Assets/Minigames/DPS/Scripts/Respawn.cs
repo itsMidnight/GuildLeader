13c7a81a44765a97752d43fed1b69bb333447eee
Alex Gorevski <algore@microsoft.com>
2016-03-27 12:10:53 -0700

    Move Character prefabs/scripts up one dir. Improve Respawn logic.

--- Assets/Minigames/DPS/Scripts/Respawn.cs
+++ Assets/Minigames/DPS/Scripts/Respawn.cs
@@ -16,18 +16,10 @@ public class Respawn : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D coll)
     {
-        var item = coll.GetComponent<Character>();
-        lock (item)
-        {
-            coll.enabled = false;   
-            var gObj = Instantiate<GameObject>(enemy);
-            var nme = gObj.GetComponent<Enemy>();
-            nme.Go();
-            //nme.Go();
-            //character.Init();
-            //character.InitializeLookAndFeel(Character.CharacterSkinTone.Medium, Character.CharacterSuit.K);
-            Destroy(coll.gameObject);
-        }
-
+        coll.enabled = false;
+        var gObj = Instantiate<GameObject>(enemy);
+        var nme = gObj.GetComponent<Enemy>();
+        nme.Go();
+        Destroy(coll.gameObject);
     }
 }
