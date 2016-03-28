49f82cb0635a40cb1bfff81b2a43ec565e98f640
Nils Brasz <nbrasz@splunk.com>
2016-03-28 00:19:28 -0700

    Adding character panel and card prefabs

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -11,19 +11,26 @@ public class PanelController : MonoBehaviour
 	Sprite [] sprites;
 	string[] names1 = {"Dark", "Haunted", "Eerie", "Blasted", "Forgotten", "Dragon's", "Mage's"};
 	string[] names2 = {"Forest", "Keep", "Castle", "Mountain", "Desert", "Tower", "Swamp"};
+	string[] names3 = {"Omisu", "Antiok", "Graey", "Felith", "Fish123", "xX_n00bKiller_Xx", "testbutts"};
 	public RectTransform containerRectTransform;
+	public string panelType;
 	// Use this for initialization
 
 	void Start () 
 	{
 		Debug.Log ("start");
-
-
-		sprites = Resources.LoadAll<Sprite>("Sprites/RaidIcons");
-
-
 		containerRectTransform = gameObject.GetComponent<RectTransform>();
-		for (int i = 0; i < 5; i++) {
+		if (panelType == "Raid") {
+			sprites = Resources.LoadAll<Sprite> ("Sprites/RaidIcons");
+			AddCard ();
+		}
+		if (panelType == "Character") 
+		{
+			//get random dude
+		}
+
+
+		for (int i = 0; i < 4; i++) {
 			AddCard ();
 		}
 
@@ -46,16 +53,35 @@ public class PanelController : MonoBehaviour
 		float ratio = width / rectTrans.rect.width;
 		float height = rectTrans.rect.height * ratio;
 
-		//Populate Raid Info Here
-		string name = names1[(int)Random.Range(0f,(float)names1.Length)]+" " + names2[(int)Random.Range(0f,(float)names2.Length)];
-		newCard.name = name + " Card";
-		GameObject IconObj = newCard.transform.FindChild("Icon").gameObject;
-		GameObject TitleObj = newCard.transform.FindChild("Title").gameObject;
-		Image iconImg = IconObj.GetComponent<Image> ();
-		Text titleText = TitleObj.GetComponent<Text> ();
+		if (panelType == "Raid") 
+		{
+			//Populate Raid Info Here
+			string name = names1 [(int)Random.Range (0f, (float)names1.Length)] + " " + names2 [(int)Random.Range (0f, (float)names2.Length)];
+			newCard.name = name + " Card";
+			GameObject IconObj = newCard.transform.FindChild ("Icon").gameObject;
+			GameObject TitleObj = newCard.transform.FindChild ("Title").gameObject;
+			Image iconImg = IconObj.GetComponent<Image> ();
+			Text titleText = TitleObj.GetComponent<Text> ();
 
-		titleText.text = name;
-		iconImg.sprite = sprites[(int)Random.Range(0f,(float)sprites.Length)];
+			titleText.text = name;
+			iconImg.sprite = sprites[(int)Random.Range(0f,(float)sprites.Length)];
+		}
+
+		if (panelType == "Character") 
+		{
+			//Populate Character Info Here
+			string name = names3 [(int)Random.Range (0f, (float)names3.Length)];
+			newCard.name = name + " Card";
+			GameObject CharacternObj = newCard.transform.FindChild ("Character").gameObject;
+			GameObject NameObj = newCard.transform.FindChild ("Name").gameObject;
+			Text nameText = NameObj.GetComponent<Text> ();
+
+			//update character here
+
+			nameText.text = name;
+		}
+
+
 
 		float x = 0;
 		float y = 0 - height * num_cards;
@@ -68,5 +94,13 @@ public class PanelController : MonoBehaviour
 		rectTrans.localScale = new Vector3(1, 1, 1);
 
 		num_cards++;
+	}
+
+	void FadeAll(GameObject exclude)
+	{
+		/*Renderer GetComponentsInChildren<Renderer>();
+		foreach (HingeJoint joint in hingeJoints) {
+			joint.useSpring = false;
+		}*/
 	}
 }
