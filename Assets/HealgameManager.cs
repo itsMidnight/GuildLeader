1d4fcfa24d8e97d55118408bad98bd7b903f215a
David Lowe <daviddevs@outlook.com>
2016-03-27 22:31:17 -0700

    Missed some files

---
+++ Assets/HealgameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealgameManager : MonoBehaviour
+{
+    public GameObject objec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SpawnCharacter()
+    {
+
+    }
+
+    public void RemoveCharacter()
+    {
+
+    }
+}
